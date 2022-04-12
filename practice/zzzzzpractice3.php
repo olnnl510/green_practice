59f4eae6f8f9d388a59f2f96f1ee48ed76c02e7c
geniusy <101864135+olnnl510@users.noreply.github.com>
2022-04-12 17:42:55 +0900

    Add files via upload

---
+++ practice/zzzzzpractice3.php
@@ -0,0 +1,11 @@
+<?php
+    for ($i=2; $i<=9; $i++)
+    {
+        for ($z=1; $z<=9; $z++)
+        {
+            $result = $i * $z ;
+            print " $i x $z = $result <br> " ;
+        }
+        print " --------------------<br> ";
+    }
+?>
