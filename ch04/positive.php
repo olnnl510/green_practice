f5823ad567db3f6d7cb2df4579ad014aa25ae0df
geniusy <101864135+olnnl510@users.noreply.github.com>
2022-04-15 17:10:16 +0900

    Add files via upload

--- ch04/positive.php
+++ ch04/positive.php
@@ -2,7 +2,7 @@
     $result = 10;
     if($result > 0)
     {
-        $result *= 0.2;
+        $result *= 0.5;
         print "$result <br>";
         print "Positive <br>";
     }
@@ -23,5 +23,10 @@
     $result = $result /2;
     $result = $result * 0.5;
     $result *= 0.5;
+
+    Q $result 값에 -1 넣으면 Negative 뜨는 이유?
+    음수라서..? 왜?
+    *0.5 먹히지 않아서?????
+    반으로 쪼개지지 않아서?
 
 -->
