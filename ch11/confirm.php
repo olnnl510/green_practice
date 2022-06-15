4230fd6f4d4b610db61ebd1ee2ee7c26056e381a
olnnl510 <101864135+olnnl510@users.noreply.github.com>
2022-06-15 17:44:47 +0900

    Delete ch11 directory

--- ch11/confirm.php
+++
@@ -1,3 +0,0 @@
-<?php
-    echo $_SESSION['var1'], "<br>"; // destroy 됨.
-    echo $_SESSION['var2'], "<br>";
--- ch11/cookie1.php
+++
@@ -1,9 +0,0 @@
-<?php
-    setcookie("country", "Korea"); // 바로 쿠키함수 쓸수없음.. 다음페이지에서
-
-    if(isset($_COOKIE['country']))
-    {
-        echo "Country : ", $_COOKIE['country'], "<br>";
-    }
-?>
-<a href="cookie2.php">Next Page</a>
